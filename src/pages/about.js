78e833de4bfdfc899ff780d33ce8b6f5a7ced752
Preston So <pso@post.harvard.edu>
2021-10-01 18:35:06 -0300

    Add layout component to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,15 @@
 import React from "react"
 import { Link } from "gatsby"
 import Footer from "../components/footer"
+import Layout from "../components/layout"
 
 export default function About() {
   return (
-    <div>
+    <Layout>
       <h1>About us</h1>
       <p>Welcome to our about page! Here is some info about us.</p>
       <p><Link to="/">Home</Link></p>
       <Footer copyrightYear="2020" />
-    </div>
+    </Layout>
   )
 }
