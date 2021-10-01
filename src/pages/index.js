980a1183e90f51ffc8fff60ebd5309c3518f50dd
Preston So <pso@post.harvard.edu>
2021-10-01 18:29:56 -0300

    Add link to about page on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
 import Footer from "../components/footer"
 
 export default function Home() {
   return (
     <div>
       <p>Hello world!</p>
+      <p><Link to="/about/">About</Link></p>
       <Footer copyrightYear="2020" />
     </div>
   )
