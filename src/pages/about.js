ee18ea54a1a37bbd2a36216eeedad5121ddf0fd4
Preston So <pso@post.harvard.edu>
2021-10-01 18:10:41 -0300

    Add footer component to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from "react"
+import Footer from "../components/footer"
 
 export default function About() {
   return (
     <div>
       <h1>About us</h1>
       <p>Welcome to our about page! Here is some info about us.</p>
+      <Footer />
     </div>
   )
 }
