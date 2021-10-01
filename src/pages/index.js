cbe6536987ddd0f75af7dda2000b7c64080492d0
Preston So <pso@post.harvard.edu>
2021-10-01 18:34:34 -0300

    Add layout component to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import React from "react"
 import { Link } from "gatsby"
 import Footer from "../components/footer"
+import Layout from "../components/layout"
 
 export default function Home() {
   return (
-    <div>
+    <Layout>
       <p>Hello world!</p>
       <p><Link to="/about/">About</Link></p>
       <Footer copyrightYear="2020" />
-    </div>
+    </Layout>
   )
 }
