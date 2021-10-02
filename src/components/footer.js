143d3099a47c80f48a4febc80a6f05f83dba8345
Preston So <pso@post.harvard.edu>
2021-10-02 10:01:38 -0300

    Add and use CSS Modules stylesheet

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,6 @@
 import React from "react"
+import * as footerStyles from "./footer.module.css"
 
 export default function Footer(props) {
-  return <p>&copy; {props.copyrightYear} My Gatsby Site. All rights reserved.</p>
+  return <p className={footerStyles.footer}>&copy; {props.copyrightYear} My Gatsby Site. All rights reserved.</p>
 }
